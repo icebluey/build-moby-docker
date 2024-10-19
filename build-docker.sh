d65fa7b23b1cce6605e92baaae9de40caca6a52c
Kenneth Davidson <xylene2016@protonmail.com>
2024-10-19 11:46:30 -0400

    Update build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -274,7 +274,7 @@ echo
 sleep 2
 sha256sum "docker-only-${_docker_ver}-1_amd64.tar.xz" > "docker-only-${_docker_ver}-1_amd64.tar.xz".sha256
 sleep 2
-rm -fr usr etc
+/bin/rm -fr usr etc
 echo
 echo '/tmp/_output_assets:'
 /bin/ls -la --color /tmp/_output_assets
